20f33f7e519c648b1444f012f16ccaa367f7f5b3
Valeriaguerrero16 <adriana.guerrero2026@hilasal.superate.org.sv>
2024-10-05 23:12:47 -0600

    Update maon.js

--- maon.js
+++ maon.js
@@ -1,5 +1,5 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Previene el envío del formulario por defecto
+    event.preventDefault(); 
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
@@ -9,7 +9,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
         errorMessage.textContent = "Por favor, complete todos los campos.";
     } else {
         errorMessage.textContent = "";
-        // Aquí puedes agregar la lógica para autenticar al usuario
+        
         alert("Inicio de sesión exitoso");
     }
 });
